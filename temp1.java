306e615dbf444d4774db8dcc77b87ef940eb7b3b
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-29 14:41:42 +0400

    Update temp1.java

--- temp1.java
+++ temp1.java
@@ -1,12 +1,23 @@
-public class temp1{
-	public static void main(String[] arg){
-		int [] a= {10, -9, 15, -5, 20, 0, -2, 6, -1, 14};
-		int count=0;
-		for(int i:a){
-			if (i<0){
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
+public class temp1 {
+  public static void main(String[] arg) {
+    int[] a = {
+      10,
+      -9,
+      15,
+      -5,
+      20,
+      0,
+      -2,
+      6,
+      -1,
+      14
+    };
+    int count = 0;
+    for (int i: a) {
+      if (i < 0) {
+        count++;
+      }
+    }
+    System.out.println(count);
+  }
 }
